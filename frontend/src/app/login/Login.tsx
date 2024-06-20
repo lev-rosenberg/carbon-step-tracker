d5fa2ea12768ba34ecaf8198d4320a883973e79a
Lev Rosenberg <lev.rosenberg@gmail.com>
2024-06-20 14:54:37 -0500

    signup

--- frontend/src/app/login/Login.tsx
+++ frontend/src/app/login/Login.tsx
@@ -8,30 +8,44 @@ import axios from "axios";
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(false);
+  const [error, setError] = useState("");
+  const [signup, setSignup] = useState(false); //
   const { dispatch } = useContext(Context);
-  const url = process.env.NEXT_PUBLIC_BASE_URL + "/auth";
   const router = useRouter();
 
   async function handleLogin() {
+    const url = process.env.NEXT_PUBLIC_BASE_URL + "/auth";
     const data = { username: username, password: password, duration: 720 };
-    console.log("login clicked");
     try {
       const result = await axios.post(url, data);
       if (result.status === 200) {
-        setError(false);
+        setError("");
         localStorage.setItem("carbon-auth-token", result.data);
         router.push("/home");
         dispatch({ type: "SET_LOGGED_IN", payload: true });
       } else {
-        setError(true);
-        console.error("Error logging in", error);
+        setError("Login failed. Please check credentials and try again.");
       }
     } catch (error) {
-      setError(true);
-      console.error("Error logging in", error);
+      setError("Login failed. Please check credentials and try again.");
     }
   }
+
+  async function handleSignup() {
+    const url = process.env.NEXT_PUBLIC_BASE_URL + "/signup";
+    const data = { username: username, password: password };
+    try {
+      const result = await axios.post(url, data);
+      if (result.status === 200) {
+        setError("");
+        setSignup(true);
+      }
+    } catch (error) {
+      setError("Signup failed. Please try again.");
+      setSignup(false);
+    }
+  }
+
   return (
     <div className={styles.login}>
       <h1>Carbon Footprint Tracker</h1>
@@ -54,12 +68,19 @@ export default function Login() {
         />
 
         <button onClick={handleLogin}>Login</button>
-        <div className={styles.error}>
-          {error && (
-            <p>Login failed. Please check credentials and try again.</p>
-          )}
-        </div>
+        <button className={styles.signup} onClick={handleSignup}>
+          Signup
+        </button>
       </form>
+
+      <div>
+        {signup && (
+          <p className={styles.signupSuccess}>
+            Signup successful. Please login with your new credentials.
+          </p>
+        )}
+        {error && <p className={styles.error}>{error}</p>}
+      </div>
     </div>
   );
 }
